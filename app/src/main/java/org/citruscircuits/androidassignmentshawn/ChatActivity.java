9c2b5da1be706b794b68b7e4703153b2e935511e
Bratsky <saidniezoc@gmail.com>
2016-12-08 20:48:05 -0800

    l

--- app/src/main/java/org/citruscircuits/androidassignmentshawn/ChatActivity.java
+++ app/src/main/java/org/citruscircuits/androidassignmentshawn/ChatActivity.java
@@ -1,13 +1,100 @@
 package org.citruscircuits.androidassignmentshawn;
 
+import android.content.Context;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ChatActivity extends AppCompatActivity {
-
+ListView ChatList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ChatList = (ListView) findViewById(R.id.ChatList);
+    }
+    public void Messaging () {
+        final ArrayList foodList = new ArrayList();
+        foodList.add("Chips");
+        foodList.add("Electricity");
+        foodList.add("Lost Souls");
+        foodList.add("Wires");
+        foodList.add("Disks");
+        ChatList.setAdapter(new ListAdapter() {
+
+            @Override
+            public boolean areAllItemsEnabled() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled(int position) {
+                return false;
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return foodList.size();
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return foodList.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                if (view == null){
+                    view = layoutInflater.inflate(android.R.layout.simple_list_item_1, viewGroup,false);
+                }
+                TextView Message = (TextView) view.findViewById(android.R.id.text1);
+                Message.setText();
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                return 0;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        });
     }
 }
+
